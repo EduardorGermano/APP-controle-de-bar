3b9717e83074cbdfc3a6b87fb69a8989f11169d0
Eduardo <eduardor.germano@hotmail.com>
2024-02-22 22:07:53 -0300

    Realização das atividades

--- lib/aula_inicial/aula.dart
+++ lib/aula_inicial/aula.dart
@@ -1,12 +1,26 @@
 void main() {
-  String disciplina = 'Desenvolviment para Dispositivos Móveis (DDM)';
+  String disciplina = 'Desenvolvimento para Dispositivos Móveis (DDM)';
   double cargaHoraria = 90.50;
   String nomeProfessor = 'Helio';
   String sobrenomeProfesor = 'Kamakau';
   bool eAtiva = true;
   int quantidadeAlunos = 26;
 
-  print('Aula de: ' + disciplina);
+  print('Aula de ' + disciplina);
   print('Professor: ' + nomeProfessor + ' ' + sobrenomeProfesor);
-  print('Quantidade de Alunos' + '${quantidadeAlunos}');
+  print('Quantidade de Alunos: ' + '${quantidadeAlunos}');
+
+  var nomeAluno = 'Eduardo';
+  var registroAcademico = 20240001022;
+  var altura = 1.65;
+  var alunoAtivo = true;
+
+  print('Nome do Aluno ' + nomeAluno);
+  print('${registroAcademico}' +
+      ' do aluno ' +
+      '${nomeAluno}' +
+      ' esta ativo ' +
+      '${alunoAtivo}');
+
+      
 }
