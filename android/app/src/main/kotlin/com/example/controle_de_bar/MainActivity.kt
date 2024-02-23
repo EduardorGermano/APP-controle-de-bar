aba60a9dd67439e63fb451691705963b796ff0fa
Eduardo <eduardor.germano@hotmail.com>
2024-02-22 21:21:02 -0300

    Inicio do projeto

---
+++ android/app/src/main/kotlin/com/example/controle_de_bar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.controle_de_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/aula_inicial/aula.dart
@@ -0,0 +1,12 @@
+void main() {
+  String disciplina = 'Desenvolviment para Dispositivos Móveis (DDM)';
+  double cargaHoraria = 90.50;
+  String nomeProfessor = 'Helio';
+  String sobrenomeProfesor = 'Kamakau';
+  bool eAtiva = true;
+  int quantidadeAlunos = 26;
+
+  print('Aula de: ' + disciplina);
+  print('Professor: ' + nomeProfessor + ' ' + sobrenomeProfesor);
+  print('Quantidade de Alunos' + '${quantidadeAlunos}');
+}
